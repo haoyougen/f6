ae6fbb9f5d137383b9644c1553c9b0795ea18165
sgwang <sgwang@cn.ibm.com>
2016-02-22 09:29:13 +0800

    add one sql file

--- src/main/resources/db/Sys_Resources.sql
+++ src/main/resources/db/Sys_Resources.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `Sys_User` (
+CREATE TABLE `Sys_Resources` (
   `Res_Code` varchar(30) NOT NULL COMMENT '资源编码',
   `Res_Name` varchar(100) DEFAULT NULL COMMENT '资源名称',
   `Res_Type` varchar(30) DEFAULT NULL COMMENT '类型',
@@ -8,5 +8,5 @@ CREATE TABLE `Sys_User` (
   `Date_Create` datetime DEFAULT NULL COMMENT '创建时间',
   `Ur_Alter` varchar(30) DEFAULT NULL COMMENT '修改者',
   `Date_Alter` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`User_Code`)
+  PRIMARY KEY (`Res_Code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资源表';
